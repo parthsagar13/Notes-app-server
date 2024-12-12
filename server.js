9956ddd2d32e57eee052dd7b428191f6f97205d7
parthsagar13 <109973046+parthsagar13@users.noreply.github.com>
2024-12-12 12:20:54 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// Importing Modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Inititating Express
+const app = express();
+
+// Environment Variables
+require("dotenv").config();
+
+// Connecting to Database
+mongoose
+  .connect("mongodb+srv://kalathiyaparth744:YSdNweJGPWC8wind@cluster0.xun8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) =>
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Connection to the Database was established!");
+    })
+  )
+  .catch((error) => console.log(error));
+
+// Middlewares
+app.use(express.json()); // JSON Parser
+app.use(express.urlencoded({ extended: true })); // URL Body Parser
+
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    // credentials: true,
+  })
+);
+
+// Routes
+const routes = require("./routes/routes");
+app.use(routes);
